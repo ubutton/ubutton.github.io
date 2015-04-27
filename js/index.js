88313ffbe665af2d5e5014126ab5c1947e2bf307
Kyle <kylelam@me.com>
2015-04-26 21:19:00 -0700

    filled in convertAddress

--- js/index.js
+++ js/index.js
@@ -4,14 +4,26 @@
 //	generate html code for button using template
 //	change layout and display the html code, as well as a google maps for the lat lng
 //
+// note: coding style: ALLCAPS for final values
+// note: coding style: variables should be as descriptive as possible
+
+
+var DIVID="map-canvas";
+
 
 function buttonClicked(){
 	//this function get called when user press the button
 	//	entry point of this js
 	//	will first convertAddress, buildHTML, and change layout + displayMap
+	var inputAddress=document.getElementById('inputAddress').value;
+	if ((inputAddress!=null)&&(inputAddress!="")){
 
+		convertAddress(inputAddress);	
+	} else {
+		displayError("Address can not be empty");
 
-	displayMap(37.419938, -122.083479, "Atom Cafe, Charleston Road, Mountain View, CA", "map-canvas")
+	}
+
 }
 
 function displayError(errorMessage){
@@ -23,7 +35,22 @@ function displayError(errorMessage){
 
 function convertAddress(stringAddress){
 	//this function convert stringAddress into lat lng
-	return { "lat":123.45, "lng":56.789 };
+	geocoder = new google.maps.Geocoder();
+	var address = stringAddress;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+
+			//displayMap(37.419938, -122.083479, "Atom Cafe, Charleston Road, Mountain View, CA", DIVID);
+
+			dLat=results[0].geometry.location.lat();
+			dLng=results[0].geometry.location.lng();
+
+			displayMap(dLat, dLng, stringAddress, DIVID);
+		} else {
+			displayError('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+
 }
 
 function buildHTML(templateFile, lat, lng, stringAddress){
