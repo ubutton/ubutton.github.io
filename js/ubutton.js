ca49491a12bf65dbddef7c970ccce0cd7e13942d
Kyle <kylelam@me.com>
2015-05-06 19:17:34 -0700

    \"

--- js/ubutton.js
+++ js/ubutton.js
@@ -189,7 +189,7 @@ function onButtonClicked(dropoff_address, dropoff_latitude, dropoff_longitude, p
 	var baseIOS = "uber://?";
 
 	var uberURI = baseIOS + "client_id="+uberClientId+
-									"&dropoff_address="+dropoff_address+
+									"&dropoff_address=\""+dropoff_address+"\""
 									"&dropoff_latitude="+dropoff_latitude+
 									"&dropoff_longitude="+dropoff_longitude+
 									"&pickup_latitude="+pickup_latitude+
